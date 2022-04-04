c77ca4ff32d90374f7e13b6fc9e07941a31308de
Jason Reilly <jdr0887@gmail.com>
2022-04-04 10:29:57 -0400

    using docker volumes, no need to chown

--- restart.sh
+++ restart.sh
@@ -2,9 +2,9 @@
 
 export $(cat .env|grep -v '^#')
 docker-compose -f docker-compose.yml down --remove-orphans
-sudo chown -R ${USER} data
-if [ `docker network ls | awk '{print $2}'|grep -w fuse` ] ;
-then
+CMD=`docker network ls | awk '{print $2}' | grep -w fuse | head -1`
+RET=$CMD
+if [ "$RET" == "fuse" ]; then
     echo "found fuse, joining network";
 else
     echo "creating and joining fuse network";
