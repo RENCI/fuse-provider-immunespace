88280fd7a7f4a31a81678f875547d59db81c3fef
Jason Reilly <jdr0887@gmail.com>
2022-05-18 16:56:48 -0400

    using objects from fuse-cdm

--- src/main/python/fuse/models/Objects.py
+++ src/main/python/fuse/models/Objects.py
@@ -1,107 +1,27 @@
-from typing import List, Optional, Any
+from typing import Optional
 
-from fuse_utilities.main import as_form, DataType, FileType
-from pydantic import BaseModel, Field
-from pydantic.networks import EmailStr
+from fuse_utilities.main import AccessMethods, Contents, Checksums
+from pydantic import BaseModel
 
 
-@as_form
-class PluginParameter(BaseModel):
+class ProviderResponse(BaseModel):
     id: str
-    title: str
-    description: str
-    value: str
-    type: str
-    format: str
-
-
-@as_form
-class SampleVariable(BaseModel):
-    id: str = "FUSE:ExampleId"
-    title: str = "Demo sampleVariable"
-    description: str = "This sample variable is for demonstration only. Replace this with variables your appliance supports for the digital objects in your system."
-    type: str = "string"
-    default: str = "example default value"
-
-
-class Checksums(BaseModel):
-    def __init__(self, checksum: str, type: str, **data: Any):
-        super().__init__(**data)
-        self.checksum = checksum
-        self.type = type
-
-
-# access_url: AccessURL = {
-#     "url": "string",
-#     "headers": "Authorization: Basic Z2E0Z2g6ZHJz"
-# }
-class AccessURL:
-    def __init__(self, url: str, headers: str):
-        self.url = url
-        self.headers = headers
-
-
-class AccessMethods:
-    def __init__(self, type: str, access_id: str, region: str):
-        self.type = type
-        self.access_id = access_id
-        self.region = region
-
-
-class Contents:
-    def __init__(self, id: str, name: str, drs_uri: str, contents: Optional[list[str]] = None):
-        self.id = id
-        self.name = name
-        self.drs_uri = drs_uri
-        self.contents = contents
-
-
-class ImmunespaceProviderResponse:
-    def __init__(self, id: str, object_id: str, submitter_id: str, name: str, self_uri: str, created_time: str, mime_type: str, file_type: str, status: str,
-                 description: Optional[str] = None,
-                 size: Optional[int] = 0, updated_time: Optional[str] = None, version: Optional[str] = None, aliases: Optional[list[str]] = None,
-                 checksums: Optional[list[Checksums]] = None,
-                 access_methods: Optional[list[AccessMethods]] = None, contents: Optional[list[Contents]] = None, data_type: Optional[str] = None, stderr: Optional[str] = None):
-        self.id = id
-        self.object_id = object_id
-        self.submitter_id = submitter_id
-        self.name = name
-        self.mime_type = mime_type
-        self.data_type = data_type
-        self.file_type = file_type
-        self.status = status
-        self.description = description
-        self.self_uri = self_uri
-        self.size = size
-        self.created_time = created_time
-        self.updated_time = updated_time
-        self.version = version
-        self.aliases = aliases
-        self.checksums = checksums
-        self.access_methods = access_methods
-        self.contents = contents
-        self.stderr = stderr
-
-
-@as_form
-class Passports(BaseModel):
-    expand: bool = False
-    passports: List[str] = ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnYTRnaF9wYXNzcG9ydF92MSI6W119.JJ5rN0ktP0qwyZmIPpxmF_p7JsxAZH6L6brUxtad3CM"]
-
-
-@as_form
-class ProviderParameters(BaseModel):
-    service_id: str = Field(..., title="Provider service id", description="id of service used to upload this object")
-    submitter_id: EmailStr = Field(..., title="email", description="unique submitter id (email)")
-    data_type: DataType = Field(..., title="Data type of this object", description="the type of data associated with this object (e.g, results or input dataset)")
-    file_type: FileType = Field(..., description="the type of file")
-    description: Optional[str] = Field(None, title="Description", description="detailed description of this data (optional)")
-    version: Optional[str] = Field(None, title="Version of this object",
-                                   description="objects shouldn't ever be deleted unless data are redacted or there is a database consistency problem.")
-    accession_id: Optional[str] = Field(None, title="External accession ID", description="if sourced from a 3rd party, this is the accession ID on that db")
-    apikey: Optional[str] = Field(None, title="External apikey", description="if sourced from a 3rd party, this is the apikey used for retrieval")
-    aliases: Optional[str] = Field(None, title="Optional list of aliases for this object")
-    checksums: Optional[List[Checksums]] = Field(None, title="Optional checksums for the object",
-                                                 description="enables verification checking by clients; this is a json list of objects, each object contains 'checksum' and 'type' fields, where 'type' might be 'sha-256' for example.")
-    requested_object_id: Optional[str] = Field(default=None,
-                                               description="optional argument to be used by submitter to request an object_id; this could be, for example, used to retrieve objects from a 3rd party for which this endpoint is a proxy. The requested object_id is not guaranteed, enduser should check return value for final object_id used.")
+    object_id: str
+    submitter_id: str
+    name: str
+    self_uri: str
+    created_time: str
+    mime_type: str
+    file_type: str
+    status: str
+    description: Optional[str] = None,
+    size: Optional[int] = 0,
+    dimensions: Optional[str] = None,
+    updated_time: Optional[str] = None,
+    version: Optional[str] = None,
+    aliases: Optional[list[str]] = None,
+    checksums: Optional[list[Checksums]] = None,
+    access_methods: Optional[list[AccessMethods]] = None,
+    contents: Optional[list[Contents]] = None,
+    data_type: Optional[str] = None,
+    stderr: Optional[str] = None
